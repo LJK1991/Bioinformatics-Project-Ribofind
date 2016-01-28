83bcfc692c2d308ab24f7e6b703ad8115e5f20f9
Lucas <lucas.kuijpers91@gmail.com>
2016-01-28 15:21:01 +0100

    Update CombiningLocusFiles.py

--- CombiningLocusFiles.py
+++ CombiningLocusFiles.py
@@ -3,19 +3,19 @@
 import csv
 import os
 
-LocusList=os.popen('cut -f 5 /home/flucas/Documents/Bioinformatics/week3/Startfile/Riboswitches\ Lucas.csv').read().split()
+LocusList=os.popen('cut -f 5 /home/manager/Bionformatics-Project-Ribofind/Startfile/Riboswitches\ Lucas.csv').read().split()
 LocusList.pop(0)
 
-L2file = open('/home/flucas/Documents/Bioinformatics/week3/Locusfile2.txt', 'r')
-L1file = open('/home/flucas/Documents/Bioinformatics/week3/LocusFile.txt', 'r')
+L2file = open('/home/manager/Bionformatics-Project-Ribofind/Locusfile2.txt', 'r')
+L1file = open('/home/manager/Bionformatics-Project-Ribofind/LocusFile.txt', 'r')
 
 L2List = L2file.readlines()
 L1List = L1file.readlines()
 
 print(len(L1List), len(L2List), len(LocusList))
-
+os.mkdir('/home/manager/Bionformatics-Project-Ribofind/Riboswitch_sequences')
 for i in range(0,len(L1List),2):
-    newfile = open("/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/{0}.fas".format(LocusList[i//2]), 'w')
+    newfile = open("/home/manager/Bionformatics-Project-Ribofind/Riboswitch_sequences/{0}.fas".format(LocusList[i//2]), 'w')
     newfile.write(L1List[i].replace("\n",""))
     newfile.write(L2List[i])
     newfile.write(L2List[i+1].replace("\n",""))
