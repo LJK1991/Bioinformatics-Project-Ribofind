f4997aa19b7054640e1f22e7abd8554ae51d04eb
Lucas <lucas.kuijpers91@gmail.com>
2016-01-28 15:26:03 +0100

    Update runMfold.py

--- runMfold.py
+++ runMfold.py
@@ -2,16 +2,16 @@
 
 import os
 #becarefull, laptop can handle only so much, do it one per time not all at the same time, if so go to the peregrine.'''
-SeqList = os.popen('ls -p /home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079 | grep -v /').read().split()
+SeqList = os.popen('ls -p /home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079 | grep -v /').read().split()
 #make folder for each .fas with the results of mfold in them.'''
 print(os.getcwd())
-os.chdir("/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079")
+os.chdir("/home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079")
 print(os.getcwd())
 for i in range(len(SeqList)):
-    os.mkdir('/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079/{0}'.format(SeqList[i].replace(".fas","")))
-    os.chdir("/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079/{0}".format(SeqList[i].replace(".fas","")))
+    os.mkdir('/home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079/{0}'.format(SeqList[i].replace(".fas","")))
+    os.chdir("/home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079/{0}".format(SeqList[i].replace(".fas","")))
     print(os.getcwd())
-    os.popen("mfold SEQ='/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079/{0}' T=30".format(SeqList[i]),'w')
-    os.chdir("/home/flucas/Documents/Bioinformatics/week3/Riboswitch_sequences/llmg_0079")
+    os.popen("mfold SEQ='/home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079/{0}' T=30".format(SeqList[i]),'w')
+    os.chdir("/home/manager/Bioinformatics-Project-Ribofind/Riboswitch_sequences/llmg_0079")
     print(os.getcwd())
 
